6499236663a7eb6717d59a6ea73d7ac5afd01f40
Vince Pagaddut <vinzhufalar@gmail.com>
2024-01-24 14:33:39 -0500

    Update groceries.js
    Added a both flag

--- SEG-3125-Module2-Grocery/scripts/groceries.js
+++ SEG-3125-Module2-Grocery/scripts/groceries.js
@@ -3,60 +3,70 @@ var products = [
         name: "broccoli",
         vegetarian: true,
         glutenFree: true,
+        both: true,
         price: 1.99
     },
     {
         name: "bread",
         vegetarian: true,
         glutenFree: false,
+        both: false,
         price: 2.35
     },
     {
         name: "salmon",
         vegetarian: false,
         glutenFree: true,
+        both: false,
         price: 10.00
     },
     {
         name: "steak",
         vegetarian: false,
         glutenFree: true,
+        both: false,
         price: 12.99
     },
     {
         name: "apples",
         vegetarian: true,
         glutenFree: true,
+        both: true,
         price: 3.49
     },
     {
         name: "rice",
         vegetarian: true,
         glutenFree: true,
+        both: true,
         price: 0.99
     },
     {
         name: "peanut butter",
         vegetarian: true,
         glutenFree: false,
+        both: false,
         price: 5.99
     },
     {
         name: "almond milk",
         vegetarian: true,
         glutenFree: true,
+        both: true,
         price: 2.99
     },
     {
         name: "chicken breast",
         vegetarian: false,
         glutenFree: true,
+        both: false,
         price: 9.50
     },
     {
         name: "cheese",
         vegetarian: true,
         glutenFree: true,
+        both: true,
         price: 4.50
     },
    
