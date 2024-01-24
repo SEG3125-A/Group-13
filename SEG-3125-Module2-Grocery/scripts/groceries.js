b4046d541aca0f7f93cab669bcbd27771d88b23b
Vince Pagaddut <vinzhufalar@gmail.com>
2024-01-24 14:09:38 -0500

    Create groceries.js

---
+++ SEG-3125-Module2-Grocery/scripts/groceries.js
@@ -0,0 +1,20 @@
+var products = [
+	{
+		name: "brocoli",
+		vegetarian: true,
+		glutenFree: true,
+		price: 1.99
+	},
+	{
+		name: "bread",
+		vegetarian: true,
+		glutenFree: false,
+		price: 2.35
+	},
+	{
+		name: "salmon",
+		vegetarian: false,
+		glutenFree: true,
+		price: 10.00
+	}
+];
