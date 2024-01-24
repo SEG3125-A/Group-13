a0db663e8626a0a63650be48f2c41f2a8274cb89
Vince Pagaddut <vinzhufalar@gmail.com>
2024-01-24 14:13:24 -0500

    Update groceries.js

--- SEG-3125-Module2-Grocery/scripts/groceries.js
+++ SEG-3125-Module2-Grocery/scripts/groceries.js
@@ -1,20 +1,63 @@
 var products = [
-	{
-		name: "brocoli",
-		vegetarian: true,
-		glutenFree: true,
-		price: 1.99
-	},
-	{
-		name: "bread",
-		vegetarian: true,
-		glutenFree: false,
-		price: 2.35
-	},
-	{
-		name: "salmon",
-		vegetarian: false,
-		glutenFree: true,
-		price: 10.00
-	}
+    {
+        name: "broccoli",
+        vegetarian: true,
+        glutenFree: true,
+        price: 1.99
+    },
+    {
+        name: "bread",
+        vegetarian: true,
+        glutenFree: false,
+        price: 2.35
+    },
+    {
+        name: "salmon",
+        vegetarian: false,
+        glutenFree: true,
+        price: 10.00
+    },
+    {
+        name: "steak",
+        vegetarian: false,
+        glutenFree: true,
+        price: 12.99
+    },
+    {
+        name: "apples",
+        vegetarian: true,
+        glutenFree: true,
+        price: 3.49
+    },
+    {
+        name: "rice",
+        vegetarian: true,
+        glutenFree: true,
+        price: 0.99
+    },
+    {
+        name: "peanut butter",
+        vegetarian: true,
+        glutenFree: false,
+        price: 5.99
+    },
+    {
+        name: "almond milk",
+        vegetarian: true,
+        glutenFree: true,
+        price: 2.99
+    },
+    {
+        name: "chicken breast",
+        vegetarian: false,
+        glutenFree: true,
+        price: 9.50
+    },
+    {
+        name: "cheese",
+        vegetarian: true,
+        glutenFree: true,
+        price: 4.50
+    },
+   
 ];
