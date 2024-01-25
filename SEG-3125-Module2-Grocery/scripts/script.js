060694ba9530e168255209a8c56e30f3f7721fd5
Samuel Dion <samuelqwe@icloud.com>
2024-01-24 20:36:09 -0500

    Load products on product page

--- SEG-3125-Module2-Grocery/scripts/script.js
+++ SEG-3125-Module2-Grocery/scripts/script.js
@@ -1,3 +1,36 @@
+function renderProductList() {
+    let displayProductElement = document.getElementById('displayProduct');
+
+    displayProductElement.innerHTML = ''; // Clear product list
+    
+    // Add each product to the product selection page
+    for (let productId in products) {
+
+        let product = products[productId];
+
+        // Create the checkbox/input element
+        let productCheckbox = document.createElement('input');
+
+        // Set the attributes to created checkbox
+        productCheckbox.type = "checkbox";
+        productCheckbox.id = productId;
+        productCheckbox.value = product.name;
+
+        // Create checkbox label
+        let productLabel = document.createElement('label');
+        productLabel.for = productId;
+
+        // Create text for checkbox label and attach to label
+        let productLabelText = document.createTextNode(product.name);
+        productLabel.appendChild(productLabelText);
+
+        // Add elements to page
+        displayProductElement.appendChild(productCheckbox);
+        displayProductElement.appendChild(productLabel);
+        displayProductElement.appendChild(document.createElement('br')); // Add a line break
+    }
+}
+
 function openInfo(evt, tabName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -16,4 +49,6 @@ function openInfo(evt, tabName) {
 document.addEventListener('DOMContentLoaded', (event) => {
     // Open the first tab by default
     document.querySelector('.tablinks').click();
+
+    renderProductList();
 });
