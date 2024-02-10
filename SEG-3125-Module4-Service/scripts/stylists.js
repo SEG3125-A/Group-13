9183a09b3b6b0784b742e948bbc40fb29f89da10
luminafaith <leah.young7061@gmail.com>
2024-02-09 20:38:02 -0500

    Create stylists.js

---
+++ SEG-3125-Module4-Service/scripts/stylists.js
@@ -0,0 +1,30 @@
+var stylist = [
+    {
+        name: "MasStyle",
+        position: "Master Stylist",
+        weekSlots: [9, 10, 11, 13, 14],
+        satSlots: [12, 13, 14, 13],
+        services: ["cut", "allover", "foil", "style"]
+    },
+    {
+        name: "SenStyle",
+        position: "Senior Stylist",
+        weekSlots: [12, 13, 14, 15],
+        satSlots: [8, 9, 10, 11],
+        services: ["cut", "foil", "style"]
+    },
+    {
+        name: "IntStyle",
+        position: "Intermediate Stylist",
+        weekSlots: [9, 10, 11, 12],
+        satSlots: [8, 9, 10, 11],
+        services: ["cut", "style"]
+    },
+    {
+        name: "MasStyle2",
+        position: "Master Stylist",
+        weekSlots: [14, 15, 16, 17, 18],
+        satSlots: [10, 11, 12, 13],
+        services: ["cut", "allover", "foil", "style"]
+    }
+]
