eda8522cdf76eabd30fb5be6e184748fddc9c96e
luminafaith <leah.young7061@gmail.com>
2024-03-03 20:47:34 -0500

    Remove excess and reorder

--- SEG-3125-Module6-SurveyAnalysis/app.js
+++ SEG-3125-Module6-SurveyAnalysis/app.js
@@ -10,24 +10,16 @@ var app = express();
 // set up template engine
 app.set('view engine', 'ejs');
 
-app.get('/survey', (req, res) => {
-    res.render('survey');
-})
-
-app.get('/analysis', (req, res) => {
-    res.render('analysis');
-})
-
 // static file serving
 app.use('/public', express.static('./public'));
+
+// fire function from surveyController
+surveyController(app);
 
 // 404 page
 app.use((req, res) => {
     res.status(404).render('404');
 })
-
-// fire function from surveyController
-surveyController(app);
 
 // listen to port
 app.listen(3000);
