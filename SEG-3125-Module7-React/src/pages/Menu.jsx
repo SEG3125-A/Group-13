7caa6faf30c50101fcd56d43878ae82ceaed5a9a
Samuel Dion <samuelqwe@icloud.com>
2024-03-17 17:11:25 -0400

    Implement categories in restaurant menu

--- SEG-3125-Module7-React/src/pages/Menu.jsx
+++ SEG-3125-Module7-React/src/pages/Menu.jsx
@@ -1,24 +1,33 @@
 import { useState } from "react";
 
 const Menu = () => {
-    const [items] = useState([
-        {id: 1, name: 'Meal Item 1', price: 20, imgUrl: 'images/chicken-alfredo-pasta.jpg'},
-        {id: 2, name: 'Meal Item 2', price: 10, imgUrl: 'images/chicken-alfredo-pasta.jpg'},
-        {id: 3, name: 'Meal Item 3', price: 15, imgUrl: 'images/chicken-alfredo-pasta.jpg'}
-    ]);
+    const [categories] = useState({
+        Appetizers:
+        [{id: 1, name: 'Meal Item 1', price: 20, desc:'Lorem ipsum', imgUrl: 'images/chicken-alfredo-pasta.jpg'},
+        {id: 2, name: 'Meal Item 2', price: 10, desc:'Lorem ipsum', imgUrl: 'images/chicken-alfredo-pasta.jpg'},
+        {id: 3, name: 'Meal Item 3', price: 15, desc:'Lorem ipsum', imgUrl: 'images/chicken-alfredo-pasta.jpg'}],
+        Entrées:
+        [{id: 1, name: 'Meal Item 4', price: 20, desc:'Lorem ipsum', imgUrl: 'images/chicken-alfredo-pasta.jpg'},
+        {id: 2, name: 'Meal Item 5', price: 10, desc:'Lorem ipsum', imgUrl: 'images/chicken-alfredo-pasta.jpg'},
+        {id: 3, name: 'Meal Item 6', price: 15, desc:'Lorem ipsum', imgUrl: 'images/chicken-alfredo-pasta.jpg'}]
+    });
     return (
         <div className="menu">
-            {items.map((item) => (
-                <div className="menu-item" key={item.id}>
-                    <div className="menu-item-pic">
-                        <img src={item.imgUrl} alt={item.name}></img>
-                        <p>${item.price}</p>
+            {Object.keys(categories).map((category) => (
+            <div className="menu-category">
+                <h1>{category}</h1>
+                {categories[category].map((item) => (
+                    <div className="menu-item" key={item.id}>
+                        <div className="menu-item-pic">
+                            <img src={item.imgUrl} alt={item.name}></img>
+                        </div>
+                        <div className="menu-item-desc">
+                            <span><h3 style={{display: 'inline'}}>{item.name}</h3> / ${item.price}</span>
+                            <p>{item.desc}</p>
+                        </div>
                     </div>
-                    <div className="menu-item-desc">
-                        <h3>{item.name}</h3>
-                        <p>Lorem ipsum</p>
-                    </div>
-                </div>
+                ))}
+            </div>
             ))}
         </div>
     );
