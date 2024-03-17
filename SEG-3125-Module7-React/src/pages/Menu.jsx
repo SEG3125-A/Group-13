5517cceb8681bfe77a9b1d322a1e54e1ff6f585d
Samuel Dion <samuelqwe@icloud.com>
2024-03-17 16:42:04 -0400

    Add basic design for restaurant menu items

--- SEG-3125-Module7-React/src/pages/Menu.jsx
+++ SEG-3125-Module7-React/src/pages/Menu.jsx
@@ -2,15 +2,22 @@ import { useState } from "react";
 
 const Menu = () => {
     const [items] = useState([
-        {id: 1, name: 'Meal Item 1', price: 20, imgUrl: ''},
-        {id: 2, name: 'Meal Item 2', price: 10, imgUrl: ''},
-        {id: 3, name: 'Meal Item 3', price: 15, imgUrl: ''}
+        {id: 1, name: 'Meal Item 1', price: 20, imgUrl: 'images/chicken-alfredo-pasta.jpg'},
+        {id: 2, name: 'Meal Item 2', price: 10, imgUrl: 'images/chicken-alfredo-pasta.jpg'},
+        {id: 3, name: 'Meal Item 3', price: 15, imgUrl: 'images/chicken-alfredo-pasta.jpg'}
     ]);
     return (
         <div className="menu">
             {items.map((item) => (
                 <div className="menu-item" key={item.id}>
-                    <p>{item.name} - ${item.price}</p>
+                    <div className="menu-item-pic">
+                        <img src={item.imgUrl} alt={item.name}></img>
+                        <p>${item.price}</p>
+                    </div>
+                    <div className="menu-item-desc">
+                        <h3>{item.name}</h3>
+                        <p>Lorem ipsum</p>
+                    </div>
                 </div>
             ))}
         </div>
