eec6013ef6eac728e7886eebae5b805c5287b617
luminafaith <leah.young7061@gmail.com>
2024-03-17 22:20:26 -0400

    Link CSS

--- SEG-3125-Module7-React/src/pages/FAQ.jsx
+++ SEG-3125-Module7-React/src/pages/FAQ.jsx
@@ -1,7 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import './FAQ.css'; // Make sure you have the corresponding CSS file
+
 const FAQ = () => {
     return (
         <div className="faq">
+            <h1>Got Questions?</h1>
             <p>This is the FAQ page.</p>
+            <h1>Still have questions?</h1>
+            <p>Click "Chat with us!" to talk to one of our staff!</p>
         </div>
     );
 }
