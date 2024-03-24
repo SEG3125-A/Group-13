1361ff7569b3d24bd3be4b2615f5f3acc34401d6
luminafaith <leah.young7061@gmail.com>
2024-03-24 16:17:59 -0400

    Create content skeleton

--- SEG-3125-Module7-React/src/pages/About.jsx
+++ SEG-3125-Module7-React/src/pages/About.jsx
@@ -1,9 +1,19 @@
 import React, { useState, useEffect, useRef   } from 'react';
-import '../components/Chat.css';
+import './About.css';
 const About = () => {
   return (
     <div className="about">
         <p>This is the about us page.</p>
+        <h1>About Us</h1>
+        <div className="ownerContainer">
+          <div className="imageLeft"><img src="images/restaurant-owner.png" alt='Restaurant owners standing in the doorway to The Big Fork with smiles.'></img></div>
+          <div className="text">Hi! We're </div>
+        </div>
+        <div className="historyContainer">
+        <div className="text">The Big Fork was established in </div>
+          <div className='imageRight'><img src="images/dining-room.jpg" alt='alt'></img></div>
+          
+        </div>
     </div>
     );
   }
